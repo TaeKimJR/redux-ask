1d7463c95e55af42ea9c7336a4c7450b9702f3cf
Tae Kim <taekimjr@gmail.com>
2018-03-04 22:20:47 -0600

    Remove uglify for testing

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+// import uglify from 'rollup-plugin-uglify';
 
 export default {
   input: 'src/index.js',
@@ -9,7 +9,6 @@ export default {
     format: 'cjs',
   },
   plugins: [
-    babel({}),
-    uglify()
+    babel({})
   ],
 };
